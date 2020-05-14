536ab61fe00ab1bd21127030d4f0384829cd0d78
’hwddzx‘ <775984360@qq.com>
2020-05-14 12:06:23 +0800

    首页轮播图

---
+++ src/views/home/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+
+    <home-swiper :banners="banners"></home-swiper>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import HomeSwiper from "./childComps/HomeSwiper";
+
+  import {getHomeMultiData} from "network/home"
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper
+    },
+    data() {
+      return{
+        banners: [],
+        recommends: [],
+      }
+    },
+    created() {
+      getHomeMultiData().then(res => {
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #f6f6f6;
+  }
+</style>
